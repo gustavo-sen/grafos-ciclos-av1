6a847ecbc94573b5feecfa349ea159e755d9b457
DaltonLinconl <daltonlinconl19@.com>
2024-09-09 15:24:38 -0300

    fix_DFS

--- DFS.py
+++ DFS.py
@@ -1,23 +1,30 @@
-tempo = 0
-ciclo = False
-cor = {}  
-descoberta = {}  
-termino = {}  
-predecessor = {}  
 
 def hasLoop(grafo):
-    global ciclo
+    # Inicializa variáveis
+    tempo = 0
+    cor = {}  
+    descoberta = {}  
+    termino = {}  
+    predecessor = {}  
+    
+    # Variável para detectar ciclos
+    ciclo = False
+
+    # Inicializa todos os vértices
     for vertice in grafo:
         cor[vertice] = 'branco'  # Todos começam como brancos
         predecessor[vertice] = None  
+
+    # Percorre todos os vértices
     for vertice in grafo:
         if cor[vertice] == 'branco':
-            ciclo = dfs_visit(grafo, vertice)
-    return ciclo
+            if dfs_visit(grafo, vertice, cor, descoberta, termino, predecessor, tempo):
+                return True
+    
+    return False
 
 
-def dfs_visit(grafo, vertice):
-    global tempo, ciclo
+def dfs_visit(grafo, vertice, cor, descoberta, termino, predecessor, tempo):
     cor[vertice] = 'cinza'  # Vértice está sendo descoberto
     tempo += 1
     descoberta[vertice] = tempo  # Tempo de descoberta
@@ -26,11 +33,14 @@ def dfs_visit(grafo, vertice):
     for adjacente in grafo[vertice]:
         if cor[adjacente] == 'branco': 
             predecessor[adjacente] = vertice
-            dfs_visit(grafo, adjacente)
+            if dfs_visit(grafo, adjacente, cor, descoberta, termino, predecessor, tempo):
+                return True
         elif cor[adjacente] == 'cinza':
-            ciclo = True       
+            # Um ciclo é detectado se você voltar a um nó "cinza"
+            return True       
     
     cor[vertice] = 'preto'  # Vértice terminado
     tempo += 1
     termino[vertice] = tempo  # Tempo de término
-    return ciclo
+    return False  # Nenhum ciclo detectado
+
