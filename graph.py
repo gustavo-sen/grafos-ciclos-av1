c01ae33e225e6d1473e54de6eaa09bce8bbdd7be
DaltonLinconl <daltonlinconl19@.com>
2024-09-09 15:09:49 -0300

    fix_removenode_&_addnode

--- graph.py
+++ graph.py
@@ -25,32 +25,34 @@ def update_plot():
     plt.pause(0.1)  # Pausa para permitir a visualização
 
 def connect_node(start_node, end_node=''):
+    
+    # Adiciona o nó de início ao grafo e ao dicionário se ele não existir
+    if start_node not in grafo:
+        grafo[start_node] = []
+        G.add_node(start_node)    
+    
     # Adiciona o nó de destino ao grafo e ao dicionário se ele não existir
     if end_node != '' and end_node not in grafo:
         grafo[end_node] = []
         G.add_node(end_node)
 
-    # Adiciona o nó de início ao grafo e ao dicionário se ele não existir
-    if start_node not in grafo:
-        grafo[start_node] = []
-        G.add_node(start_node)
-
     # Adiciona a aresta
-    if end_node:
+    if end_node and end_node not in grafo[start_node]:
         grafo[start_node].append(end_node)
-        #grafo[end_node].append(start_node) 
         G.add_edge(start_node, end_node)
+
 
 def remove_node(node_name):
     if node_name not in grafo:
         print(f"Node {node_name} not found")
         return
     
-    # Remove as arestas associadas ao nó
-    for neighbor in grafo[node_name]:
-        G.remove_edge(node_name, neighbor)
-        grafo[neighbor].remove(node_name)
-    
+    # Remove todas as arestas que têm o nó como destino
+    for other_node in grafo:
+        if node_name in grafo[other_node]:
+            G.remove_edge(other_node, node_name)
+            grafo[other_node].remove(node_name)
+
     # Remove o nó do grafo e do dicionário
     G.remove_node(node_name)
     del grafo[node_name]
