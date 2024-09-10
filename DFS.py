4dd6eb80cc48a5da901af05e3f63baaa22f2040c
Gustavo <vortex@VRP65048455.acad.unifor.br>
2024-09-10 17:10:18 -0300

    #Modify
    reduze code

--- DFS.py
+++ DFS.py
@@ -1,30 +1,30 @@
+def dfs(node, graph, visited, stack):
+    # Marcar o nó como visitado e adicioná-lo à pilha de recursão
+    visited.add(node)
+    stack.add(node)
+    
+    # Explorar todos os vizinhos
+    for neighbor in graph.get(node, []):
+        if neighbor not in visited:
+            if dfs(neighbor, graph, visited, stack):
+                return True
+        elif neighbor in stack:
+            # Se o vizinho está na pilha de recursão, encontramos um ciclo
+            return True
+    
+    # Remover o nó da pilha de recursão ao final da exploração
+    stack.remove(node)
+    return False
 
 def hasLoop(graph):
-    def dfs(node):
-        # Marcar o nó como visitado e adicioná-lo à pilha de recursão
-        visited.add(node)
-        rec_stack.add(node)
-        
-        # Explorar todos os vizinhos
-        for neighbor in graph.get(node, []):
-            if neighbor not in visited:
-                if dfs(neighbor):
-                    return True
-            elif neighbor in rec_stack:
-                # Se o vizinho está na pilha de recursão, encontramos um ciclo
-                return True
-        
-        # Remover o nó da pilha de recursão ao final da exploração
-        rec_stack.remove(node)
-        return False
-    
     visited = set()
-    rec_stack = set()
+    stack = set()
     
     # Verificar todos os nós para cobrir grafos desconexos
     for node in graph:
         if node not in visited:
-            if dfs(node):
+            if dfs(node, graph, visited, stack):
                 return True
     
     return False
+
