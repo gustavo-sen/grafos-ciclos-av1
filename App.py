df02b3520e3a5e1b460d8b9774f0ab01bfff2fe1
Gustavo <vortex@VRP65048455.acad.unifor.br>
2024-09-10 11:08:00 -0300

    #Modify
    - improve main loop

--- App.py
+++ App.py
@@ -2,6 +2,11 @@ from graph import *
 from DFS import *
 create_plot()
 
+def update_plots():
+    print(grafo)
+    print("É cíclico: ", hasLoop(grafo)) 
+    update_plot()  
+
 while True:
     command = input("Adicionar (A), Remover (R), Conectar (C), Sair (X): ").strip().upper()
 
@@ -9,22 +14,19 @@ while True:
         node_name = input("Nome do nó: ").strip().capitalize()
         if node_name:
             connect_node(node_name)
+            update_plots()
     elif command == 'R':
-        node_name = input("Nome do nó a ser removido: ").strip().capitalize()
+        node_name = input("Nome do nó: ").strip().capitalize()
         if node_name: 
             remove_node(node_name)
+            update_plots()
     elif command == "C":
         start_node = input("Nome do nó inicial: ").strip().capitalize()
         end_node = input("Nome do nó final: ").strip().capitalize()
         if start_node and end_node: 
             connect_node(start_node, end_node)
+            update_plots()
     elif command == "X":
         plt.ioff() 
         plt.close()
         break
-    else:
-        print("Comando inválido. Tente novamente.")
-
-    print(grafo)
-    print("É cíclico: ", hasLoop(grafo)) 
-    update_plot()  
