fbbcde79b095fa101eadad4d6e27d7c262dcde50
Gustavo <vortex@VRP65048455.acad.unifor.br>
2024-09-10 17:06:12 -0300

    #Modify

--- DFS.py
+++ DFS.py
@@ -1,46 +1,30 @@
 
-def hasLoop(grafo):
-    # Inicializa variáveis
-    tempo = 0
-    cor = {}  
-    descoberta = {}  
-    termino = {}  
-    predecessor = {}  
+def hasLoop(graph):
+    def dfs(node):
+        # Marcar o nó como visitado e adicioná-lo à pilha de recursão
+        visited.add(node)
+        rec_stack.add(node)
+        
+        # Explorar todos os vizinhos
+        for neighbor in graph.get(node, []):
+            if neighbor not in visited:
+                if dfs(neighbor):
+                    return True
+            elif neighbor in rec_stack:
+                # Se o vizinho está na pilha de recursão, encontramos um ciclo
+                return True
+        
+        # Remover o nó da pilha de recursão ao final da exploração
+        rec_stack.remove(node)
+        return False
     
-    # Variável para detectar ciclos
-    ciclo = False
-
-    # Inicializa todos os vértices
-    for vertice in grafo:
-        cor[vertice] = 'branco'  # Todos começam como brancos
-        predecessor[vertice] = None  
-
-    # Percorre todos os vértices
-    for vertice in grafo:
-        if cor[vertice] == 'branco':
-            if dfs_visit(grafo, vertice, cor, descoberta, termino, predecessor, tempo):
+    visited = set()
+    rec_stack = set()
+    
+    # Verificar todos os nós para cobrir grafos desconexos
+    for node in graph:
+        if node not in visited:
+            if dfs(node):
                 return True
     
     return False
-
-
-def dfs_visit(grafo, vertice, cor, descoberta, termino, predecessor, tempo):
-    cor[vertice] = 'cinza'  # Vértice está sendo descoberto
-    tempo += 1
-    descoberta[vertice] = tempo  # Tempo de descoberta
-    
-    # Explora cada adjacente
-    for adjacente in grafo[vertice]:
-        if cor[adjacente] == 'branco': 
-            predecessor[adjacente] = vertice
-            if dfs_visit(grafo, adjacente, cor, descoberta, termino, predecessor, tempo):
-                return True
-        elif cor[adjacente] == 'cinza':
-            # Um ciclo é detectado se você voltar a um nó "cinza"
-            return True       
-    
-    cor[vertice] = 'preto'  # Vértice terminado
-    tempo += 1
-    termino[vertice] = tempo  # Tempo de término
-    return False  # Nenhum ciclo detectado
-
